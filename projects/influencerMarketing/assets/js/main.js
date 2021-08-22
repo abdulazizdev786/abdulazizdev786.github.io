78b5f8b4b21ee5e033ed2d74f063e82303a90721
abdulaziz65 <abdulaziz652369@gmail.com>
2021-08-23 00:11:11 +0500

    Project Update

---
+++ projects/influencerMarketing/assets/js/main.js
@@ -0,0 +1,18 @@
+let fInfBtnNext = document.getElementById("f-inf-item-next");
+let fInfBtnPrev = document.getElementById("f-inf-item-prev");
+let fInfItem = document.querySelector(".f-influencer-items");
+let left=0;
+fInfBtnNext.addEventListener('click', function(){
+    if(left <= 0 && left > -200){
+        console.log("move left")
+    left -= 100
+    fInfItem.style.left = left+"%"
+    }
+})
+fInfBtnPrev.addEventListener('click', function(){
+    if(left>= -200 && left !=0){
+        left += 100
+        console.log("move right")
+        fInfItem.style.left = left+"%"
+    }
+})

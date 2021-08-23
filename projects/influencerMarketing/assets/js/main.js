a67e3c88d6e0153761a68adf9af0f8fd6b59ed55
abdulaziz65 <abdulaziz652369@gmail.com>
2021-08-23 12:10:19 +0500

    Project Update

--- projects/influencerMarketing/assets/js/main.js
+++ projects/influencerMarketing/assets/js/main.js
@@ -6,13 +6,19 @@ fInfBtnNext.addEventListener('click', function(){
     if(left <= 0 && left > -200){
         console.log("move left")
     left -= 100
-    fInfItem.style.left = left+"%"
     }
+    else{
+        left = 0;
+    }
+    fInfItem.style.left = left+"%"
 })
 fInfBtnPrev.addEventListener('click', function(){
     if(left>= -200 && left !=0){
         left += 100
         console.log("move right")
-        fInfItem.style.left = left+"%"
     }
+    else{
+        left = -200
+    }
+    fInfItem.style.left = left+"%"
 })

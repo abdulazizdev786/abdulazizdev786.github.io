7f43538ad59c1dac402773a1bde42873e584b665
abdulaziz65 <abdulaziz652369@gmail.com>
2021-08-25 21:33:23 +0500

    Project 2 Update

--- projects/PlatinumVIP/assets/js/main.js
+++ projects/PlatinumVIP/assets/js/main.js
@@ -50,110 +50,73 @@ class TypeWriter {
     new TypeWriter(txtElement, words, wait);
   }
 
+// carousel script 
+let btnRight = document.querySelector(".btn-carousel-next");
+let btnLeft = document.querySelector(".btn-carousel-prev");
+let left=0;
+let carousel = document.querySelector(".c-events-carousel-inner");
 
-
+btnRight.addEventListener('click',function(){
+  if($(window).width()> 612){
+    if(left <= 0 && left > -33){
+      left -= 33
+      }
+      else{
+          left = 0;
+      }
+      carousel.style.left = left+"%"
+  }
+  else{
+    if(left <= 0 && left > -300){
+      left -= 100
+      }
+      else{
+          left = 0;
+      }
+      carousel.style.left = left+"%"
+  }
   
-$(document).ready(function () {
-    var itemsMainDiv = ('.c-events-carousel');
-    var itemsDiv = ('.c-events-carousel-inner');
-    var itemWidth = "";
-
-    $('.leftLst, .rightLst').click(function () {
-        var condition = $(this).hasClass("leftLst");
-        if (condition)
-            click(0, this);
-        else
-            click(1, this)
-    });
-
-    ResCarouselSize();
-
-
-
-
-    $(window).resize(function () {
-        ResCarouselSize();
-    });
-
-    //this function define the size of the carousel items
-    function ResCarouselSize() {
-        var incno = 0;
-        var dataItems = ("data-items");
-        var itemClass = ('.c-events-item');
-        var id = 0;
-        var btnParentSb = '';
-        var itemsSplit = '';
-        var sampwidth = $(itemsMainDiv).width();
-        var bodyWidth = $('body').width();
-        $(itemsDiv).each(function () {
-            id = id + 1;
-            var itemNumbers = $(this).find(itemClass).length;
-            btnParentSb = $(this).parent().attr(dataItems);
-            itemsSplit = btnParentSb.split(',');
-            $(this).parent().attr("id", "MultiCarousel" + id);
-
-
-            if (bodyWidth >= 1200) {
-                incno = itemsSplit[1];
-                itemWidth = sampwidth / incno;
-            }
-            else if (bodyWidth >= 992) {
-                incno = itemsSplit[1];
-                itemWidth = sampwidth / incno;
-            }
-            else if (bodyWidth >= 768) {
-                incno = itemsSplit[1];
-                itemWidth = sampwidth / incno;
-            }
-            else {
-                incno = itemsSplit[0];
-                itemWidth = sampwidth / incno;
-            }
-            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
-            $(this).find(itemClass).each(function () {
-                $(this).outerWidth(itemWidth);
-            });
-
-        });
-    }
-
-
-    function ResCarousel(e, el, s) {
-        var leftBtn = ('.leftLst');
-        var rightBtn = ('.rightLst');
-        var translateXval = '';
-        var divStyle = $(el + ' ' + itemsDiv).css('transform');
-        var values = divStyle.match(/-?[\d\.]+/g);
-        var xds = Math.abs(values[4]);
-        if (e == 0) {
-            translateXval = parseInt(xds) - parseInt(itemWidth * s);
-            $(el + ' ' + rightBtn).removeClass("over");
-
-            if (translateXval <= itemWidth / 2) {
-                translateXval = 0;
-                $(el + ' ' + leftBtn).addClass("over");
-            }
-        }
-        else if (e == 1) {
-            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
-            translateXval = parseInt(xds) + parseInt(itemWidth * s);
-            $(el + ' ' + leftBtn).removeClass("over");
-
-            if (translateXval >= itemsCondition - itemWidth / 2) {
-                translateXval = itemsCondition;
-                $(el + ' ' + rightBtn).addClass("over");
-            }
-        }
-        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
-    }
-
-    function click(ell, ee) {
-        var Parent = "#" + $(ee).parent().attr("id");
-        var slide = $(Parent).attr("data-slide");
-        ResCarousel(ell, Parent, slide);
-    }
-
 });
+btnLeft.addEventListener('click', function(){
+  if($(window).width()> 612){
+    if(left>= -33 && left !=0){
+      left += 33
+  }
+  else{
+      left = 0
+  }
+  carousel.style.left = left+"%"
+  }
+  else {
+    if(left>= -300 && left !=0){
+      left += 100
+  }
+  else{
+      left = 0
+  }
+  carousel.style.left = left+"%"
+  }
+ 
+})
 
-
-
+setInterval(() => {
+  if($(window).width()> 612){
+    if(left <= 0 && left > -33){
+      left -= 33
+      }
+      else{
+          left = 0;
+      }
+      carousel.style.left = left+"%"
+  }
+  else{
+    if(left <= 0 && left > -300){
+      left -= 100
+      }
+      else{
+          left = 0;
+      }
+      carousel.style.left = left+"%"
+  }
+}, 4000);
+  

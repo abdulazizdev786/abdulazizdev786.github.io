6027ce1d1633e44ccbd61ca20be1e5df813bbcda
abdulaziz65 <abdulaziz652369@gmail.com>
2021-08-25 18:11:18 +0500

    Project 2 Update

--- projects/PlatinumVIP/assets/js/main.js
+++ projects/PlatinumVIP/assets/js/main.js
@@ -1,3 +1,58 @@
+
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+    }
+  
+    type() {
+      const current = this.wordIndex % this.words.length;
+      const fullTxt = this.words[current];
+  
+      if(this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+  
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  
+      let typeSpeed = 70;
+  
+      if(this.isDeleting) {
+        typeSpeed /= 2;
+      }
+      if(!this.isDeleting && this.txt === fullTxt) {
+        typeSpeed = this.wait;
+        this.isDeleting = true;
+      } else if(this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+        typeSpeed = 500;
+      }
+  
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
+  
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
+  function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    new TypeWriter(txtElement, words, wait);
+  }
+
+
+
+  
 $(document).ready(function () {
     var itemsMainDiv = ('.c-events-carousel');
     var itemsDiv = ('.c-events-carousel-inner');
@@ -102,54 +157,3 @@ $(document).ready(function () {
 
 
 
-
-class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-      this.txtElement = txtElement;
-      this.words = words;
-      this.txt = '';
-      this.wordIndex = 0;
-      this.wait = parseInt(wait, 10);
-      this.type();
-      this.isDeleting = false;
-    }
-  
-    type() {
-      const current = this.wordIndex % this.words.length;
-      const fullTxt = this.words[current];
-  
-      if(this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-      } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-      }
-  
-      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-  
-      let typeSpeed = 70;
-  
-      if(this.isDeleting) {
-        typeSpeed /= 2;
-      }
-      if(!this.isDeleting && this.txt === fullTxt) {
-        typeSpeed = this.wait;
-        this.isDeleting = true;
-      } else if(this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.wordIndex++;
-        typeSpeed = 500;
-      }
-  
-      setTimeout(() => this.type(), typeSpeed);
-    }
-  }
-  
-  
-  document.addEventListener('DOMContentLoaded', init);
-  
-  function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    new TypeWriter(txtElement, words, wait);
-  }
